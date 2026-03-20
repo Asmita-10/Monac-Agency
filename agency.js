3cb0e3b60e7c281800d0160d8c4acd31ee4d85a6
Asmita Tiwari <asmitatiwari2007@gmail.com>
2026-03-20 11:13:34 +0530

    Add files via upload

---
+++ agency.js
@@ -0,0 +1,7 @@
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+  });
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll();
